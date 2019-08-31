2cae88d0348e770464a316aaf7ba8c4ee9b79d4b
Eric Ma <ericmajinglong@gmail.com>
2019-08-31 10:07:57 -0400

    apply black

--- pyprojroot/pyprojroot.py
+++ pyprojroot/pyprojroot.py
@@ -6,13 +6,15 @@ def pyprojroot(p, proj_files):
     for pf in proj_files:
         found = list(p.glob(pf))
         if len(found) > 0:
-            return(p)
+            return p
     return pyprojroot(p.parent, proj_files)
 
 
-def here(rel_proj_path='.',
-         proj_files=['.git', '.here', '*.Rproj', 'requirements.txt', 'setup.py']):
-    proj_path = pyprojroot(pl.Path('.').cwd(), proj_files)
+def here(
+    rel_proj_path=".",
+    proj_files=[".git", ".here", "*.Rproj", "requirements.txt", "setup.py"],
+):
+    proj_path = pyprojroot(pl.Path(".").cwd(), proj_files)
 
     pth = proj_path / rel_proj_path
 
