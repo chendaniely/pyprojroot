9b792931b588ca566333b9ad1ef2abfb9af71151
Tranquilize <mister.raphael@gmail.com>
2019-09-13 15:46:18 +0200

    Update the required project files to be more general. Modify the function and variable naming to be more explicit.
    Add type annotations.
    Add documentation to py_project_root and here functions.

--- pyprojroot/pyprojroot.py
+++ pyprojroot/pyprojroot.py
@@ -1,23 +1,35 @@
+from typing import List
 import pathlib as pl
 import warnings
 
 
-def pyprojroot(p, proj_files):
-    for pf in proj_files:
-        found = list(p.glob(pf))
+def py_project_root(path: pl.Path, project_files: List) -> pl.Path:
+    """
+    Recursively searches for project files in the current working directory to find
+    the project root of the python project.
+    :param path: pathlib path object
+    :param project_files: list of to track project files
+    :return: pathlib path
+    """
+    for file in project_files:
+        found = list(path.glob(file))
         if len(found) > 0:
-            return(p)
-    return pyprojroot(p.parent, proj_files)
+            return path
+    return py_project_root(path.parent, project_files)
 
 
-def here(rel_proj_path='.',
-         proj_files=['.git', '.here', '*.Rproj', 'requirements.txt', 'setup.py']):
-    proj_path = pyprojroot(pl.Path('.').cwd(), proj_files)
+def here(project_files=(".git", "requirements.txt"), relative_project_path='.') -> pl.Path:
+    """
+    Returns the directory relative to the projects root directory.
+    :param project_files: list of files to track inside the project
+    :param relative_project_path:
+    :return: pathlib path
+    """
+    project_path = py_project_root(pl.Path('.').cwd(), project_files)
+    path = project_path.joinpath(relative_project_path)
 
-    pth = proj_path / rel_proj_path
-
-    if pth.exists():
-        return pth
+    if path.exists():
+        return path
     else:
-        warnings.warn("Path doesn't exist: {}".format(pth))
-        return pth
+        warnings.warn("Path doesn't exist: {}".format(path))
+        return path
