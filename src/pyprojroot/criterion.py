4ba19a67b292b9a1fb8b430cc5fd1166e12344e0
Jason Brackman <jasonbrackman@users.noreply.github.com>
2023-03-20 13:27:16 -0700

    Standardized imports naming fixes #43 (#48)

--- src/pyprojroot/criterion.py
+++ src/pyprojroot/criterion.py
@@ -5,19 +5,19 @@ See https://github.com/r-lib/rprojroot.
 It is intended for interactive or programmatic only.
 """
 
-import pathlib as _pathlib
-import typing
+from pathlib import Path
+from typing import Callable, Iterable, Union
 from os import PathLike as _PathLike
 
 
-_PathType = typing.Union[_PathLike, str]
-_CriterionType = typing.Union[
-    typing.Callable[[_PathType], bool],
-    typing.Callable[[_pathlib.Path], bool],
+_PathType = Union[_PathLike, str]
+_CriterionType = Union[
+    Callable[[_PathType], bool],
+    Callable[[Path], bool],
     _PathType,
-    _pathlib.Path,
-    typing.Iterable[typing.Callable[[_PathType], bool]],
-    typing.Iterable[typing.Callable[[_pathlib.Path], bool]],
+    Path,
+    Iterable[Callable[[_PathType], bool]],
+    Iterable[Callable[[Path], bool]],
 ]
 
 # TODO: It would be nice to have a class that encapsulates these checks,
@@ -28,7 +28,7 @@ _CriterionType = typing.Union[
 
 def as_root_criterion(
     criterion: _CriterionType,
-) -> typing.Callable[[_pathlib.Path], bool]:
+) -> Callable[[Path], bool]:
     if callable(criterion):
         return criterion
 
@@ -38,7 +38,7 @@ def as_root_criterion(
     else:
         criterion_collection = list(criterion)  # type: ignore[arg-type]
 
-    def f(path: _pathlib.Path) -> bool:
+    def f(path: Path) -> bool:
         for c in criterion_collection:
             if isinstance(c, (_PathLike, str)):
                 if (path / c).exists():
@@ -51,38 +51,38 @@ def as_root_criterion(
     return f
 
 
-def has_file(file: _PathType) -> typing.Callable[[_pathlib.Path], bool]:
+def has_file(file: _PathType) -> Callable[[Path], bool]:
     """
     Check that specified file exists in path.
 
     Note that a directory with that name will not match.
     """
 
-    def f(path: _pathlib.Path) -> bool:
+    def f(path: Path) -> bool:
         return (path / file).is_file()
 
     return f
 
 
-def has_dir(file: _PathType) -> typing.Callable[[_pathlib.Path], bool]:
+def has_dir(file: _PathType) -> Callable[[Path], bool]:
     """
     Check that specified directory exists.
 
     Note that a regular file with that name will not match.
     """
 
-    def f(path: _pathlib.Path) -> bool:
+    def f(path: Path) -> bool:
         return (path / file).is_dir()
 
     return f
 
 
-def matches_glob(pat: str) -> typing.Callable[[_pathlib.Path], bool]:
+def matches_glob(pat: str) -> Callable[[Path], bool]:
     """
     Check that glob has at least one match.
     """
 
-    def f(path: _pathlib.Path) -> bool:
+    def f(path: Path) -> bool:
         matches = path.glob(pat)
         try:
             # Only need to get one item from generator
