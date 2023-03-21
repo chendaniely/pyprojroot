67faa85ec16ff541886001e51c4d86362ee159f8
James Myatt <james.myatt@capgemini.com>
2023-03-21 09:36:28 +0000

    Remove unnecessary `__all__` from main module

--- src/pyprojroot/__init__.py
+++ src/pyprojroot/__init__.py
@@ -1,13 +1,3 @@
 from .criterion import as_root_criterion, has_dir, has_file
 from .here import here
 from .root import find_root, find_root_with_reason
-
-
-__all__ = [
-    "as_root_criterion",
-    "find_root_with_reason",
-    "find_root",
-    "has_dir",
-    "has_file",
-    "here",
-]
